fae5c36e1bfa183c4f2357164d7a01ca356be02f
Lightway <lightway29@gmail.com>
2020-08-30 22:31:52 +0530

    return_weight

--- SM-FX/src/com/saiton/ccs/stockdao/ExportStockDataDAO.java
+++ SM-FX/src/com/saiton/ccs/stockdao/ExportStockDataDAO.java
@@ -50,7 +50,7 @@ public class ExportStockDataDAO {
           
                    String query = "SELECT rl.id,rl.return_time_stamp,"
                            + "r.item_no,r.reel_number,r.item_name,"
-                           + "r.gsm,r.reel_width,rl.issue_weight "
+                           + "r.gsm,r.reel_width,rl.return_weight "
                            + "FROM reel AS r "
                            + "JOIN reel_log AS rl "
                            + "ON r.reel_code = rl.reel_code "
